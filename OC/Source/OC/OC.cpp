a79dd9fb2341a10efdf5348dcc68fa767cf48fd6
JoaoPardal <jflpardal@gmail.com>
2022-01-17 20:13:04 +0000

    Character moving with WASD
    * Using blueprint, needs cleaning

---
+++ OC/Source/OC/OC.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "OC.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, OC, "OC" );
---
+++ OC/Source/OC/OCGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "OCGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class OC_API AOCGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
