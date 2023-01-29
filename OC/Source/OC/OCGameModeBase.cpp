534cee26672f1fa098d65585d06fabf639000daf
JoaoPardal <jflpardal@gmail.com>
2023-01-29 22:15:33 +0000

    Started migrating Recipe UI element from BP to code
    * The main level BP is now using a custom cpp class to where the creation of the recipe's UI is going to be done (awful place to put this, but for now it will remain here)

--- OC/Source/OC/OCGameModeBase.cpp
+++ OC/Source/OC/OCGameModeBase.cpp
@@ -3,3 +3,41 @@
 
 #include "OCGameModeBase.h"
 
+#include "Blueprint/UserWidget.h"
+#include "Macros.h"
+#include "RequestsSubsystem.h"
+
+void AOCGameModeBase::BeginPlay()
+{
+	Super::BeginPlay();
+
+	if (auto World = GetWorld())
+	{
+		if (auto GameInstance = World->GetGameInstance())
+		{
+			auto RequestsSubsystem = GameInstance->GetSubsystem<URequestsSubsystem>();
+
+			RequestsSubsystem->OnGeneratedNewRequest.AddDynamic(this, &AOCGameModeBase::GeneratedNewRequest);
+			RequestsSubsystem->OnCompletedRequest.AddDynamic(this, &AOCGameModeBase::CompletedRequest);
+		}
+	}
+}
+
+void AOCGameModeBase::GeneratedNewRequest(const FRecipeData& GeneratedRequestData)
+{
+	if (ensureMsgf(RecipeWidget, TEXT("RecipeWidget not set in GameMode")))
+	{
+		ActiveRecipeWidget = CreateWidget(GetWorld(), RecipeWidget);
+		if (ActiveRecipeWidget)
+		{
+			// TODO set ActiveRecipeWidget's RecipeData to GeneratedRequestData
+			// this needs ActiveRecipe to be a C++ class as well
+			ActiveRecipeWidget->AddToViewport();
+		}
+	}
+}
+
+void AOCGameModeBase::CompletedRequest(FRecipeData CompletedRequestData)
+{
+	DTOS("request complete");
+}
--- OC/Source/OC/OCGameModeBase.h
+++ OC/Source/OC/OCGameModeBase.h
@@ -4,14 +4,32 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/GameModeBase.h"
+
+#include "RecipeData.h"
+
 #include "OCGameModeBase.generated.h"
 
 /**
  * 
  */
+
+class UUserWidget;
+
 UCLASS()
 class OC_API AOCGameModeBase : public AGameModeBase
 {
 	GENERATED_BODY()
-	
+public:
+	virtual void BeginPlay() override;
+
+	UFUNCTION()
+	void GeneratedNewRequest(const FRecipeData& GeneratedRequestData);
+	UFUNCTION()
+	void CompletedRequest(FRecipeData CompletedRequestData);
+
+private:
+	UPROPERTY(EditAnywhere, Category="RecipeUI")
+	TSubclassOf<UUserWidget> RecipeWidget;
+
+	UUserWidget* ActiveRecipeWidget;
 };
